f1bd89f7b28323468cf57acdd1ab0c1ba234b42e
Sreedhar Tatapudi <96693636+sree2798@users.noreply.github.com>
2023-10-24 00:44:31 -0500

    Update day1.sql

--- day1.sql
+++ day1.sql
@@ -1,1 +1,6 @@
+--Problem statement: 
+-- Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, 
+--but exclude duplicates from the answer.
+--The STATION table is described as follows:
+
 select distinct city from station where ID%2 = 0
