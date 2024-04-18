ddc7f159dfa7a76932a8647a7bf6f6b1249150a3
Sreedhar <96693636+sree2798@users.noreply.github.com>
2024-04-18 16:27:42 -0500

    Add files via upload

---
+++ Day_13.sql
@@ -0,0 +1,399 @@
+/*
+Table: Employee
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| employee_id   | int     |
+| team_id       | int     |
++---------------+---------+
+employee_id is the primary key (column with unique values) for this table.
+Each row of this table contains the ID of each employee and their respective team.
+ 
+
+Write a solution to find the team size of each of the employees.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee Table:
++-------------+------------+
+| employee_id | team_id    |
++-------------+------------+
+|     1       |     8      |
+|     2       |     8      |
+|     3       |     8      |
+|     4       |     7      |
+|     5       |     9      |
+|     6       |     9      |
++-------------+------------+
+Output: 
++-------------+------------+
+| employee_id | team_size  |
++-------------+------------+
+|     1       |     3      |
+|     2       |     3      |
+|     3       |     3      |
+|     4       |     1      |
+|     5       |     2      |
+|     6       |     2      |
++-------------+------------+
+Explanation: 
+Employees with Id 1,2,3 are part of a team with team_id = 8.
+Employee with Id 4 is part of a team with team_id = 7.
+Employees with Id 5,6 are part of a team with team_id = 9.*/
+
+SELECT e2.employee_id, COUNT(e1.employee_id) AS team_size
+FROM employee e1 CROSS JOIN employee e2
+WHERE e1.team_id = e2.team_id
+GROUP BY e2.employee_id;
+/*
+Table: Departments
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| name          | varchar |
++---------------+---------+
+In SQL, id is the primary key of this table.
+The table has information about the id of each department of a university.
+ 
+
+Table: Students
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| name          | varchar |
+| department_id | int     |
++---------------+---------+
+In SQL, id is the primary key of this table.
+The table has information about the id of each student at a university and the id of the department he/she studies at.
+ 
+
+Find the id and the name of all students who are enrolled in departments that no longer exist.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Departments table:
++------+--------------------------+
+| id   | name                     |
++------+--------------------------+
+| 1    | Electrical Engineering   |
+| 7    | Computer Engineering     |
+| 13   | Bussiness Administration |
++------+--------------------------+
+Students table:
++------+----------+---------------+
+| id   | name     | department_id |
++------+----------+---------------+
+| 23   | Alice    | 1             |
+| 1    | Bob      | 7             |
+| 5    | Jennifer | 13            |
+| 2    | John     | 14            |
+| 4    | Jasmine  | 77            |
+| 3    | Steve    | 74            |
+| 6    | Luis     | 1             |
+| 8    | Jonathan | 7             |
+| 7    | Daiana   | 33            |
+| 11   | Madelynn | 1             |
++------+----------+---------------+
+Output: 
++------+----------+
+| id   | name     |
++------+----------+
+| 2    | John     |
+| 7    | Daiana   |
+| 4    | Jasmine  |
+| 3    | Steve    |
++------+----------+
+Explanation: 
+John, Daiana, Steve, and Jasmine are enrolled in departments 14, 33, 74, and 77 respectively. 
+department 14, 33, 74, and 77 do not exist in the Departments table.*/
+select s.id,s.name from students s left join departments d
+on s.department_id=d.id WHERE d.id is null;
+/*
+Table: NPV
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| year          | int     |
+| npv           | int     |
++---------------+---------+
+(id, year) is the primary key (combination of columns with unique values) of this table.
+The table has information about the id and the year of each inventory and the corresponding net present value.
+ 
+
+Table: Queries
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| year          | int     |
++---------------+---------+
+(id, year) is the primary key (combination of columns with unique values) of this table.
+The table has information about the id and the year of each inventory query.
+ 
+
+Write a solution to find the npv of each query of the Queries table.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+NPV table:
++------+--------+--------+
+| id   | year   | npv    |
++------+--------+--------+
+| 1    | 2018   | 100    |
+| 7    | 2020   | 30     |
+| 13   | 2019   | 40     |
+| 1    | 2019   | 113    |
+| 2    | 2008   | 121    |
+| 3    | 2009   | 12     |
+| 11   | 2020   | 99     |
+| 7    | 2019   | 0      |
++------+--------+--------+
+Queries table:
++------+--------+
+| id   | year   |
++------+--------+
+| 1    | 2019   |
+| 2    | 2008   |
+| 3    | 2009   |
+| 7    | 2018   |
+| 7    | 2019   |
+| 7    | 2020   |
+| 13   | 2019   |
++------+--------+
+Output: 
++------+--------+--------+
+| id   | year   | npv    |
++------+--------+--------+
+| 1    | 2019   | 113    |
+| 2    | 2008   | 121    |
+| 3    | 2009   | 12     |
+| 7    | 2018   | 0      |
+| 7    | 2019   | 0      |
+| 7    | 2020   | 30     |
+| 13   | 2019   | 40     |
++------+--------+--------+
+Explanation: 
+The npv value of (7, 2018) is not present in the NPV table, we consider it 0.
+The npv values of all other queries can be found in the NPV table.
+*/
+/*
+Table: NPV
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| year          | int     |
+| npv           | int     |
++---------------+---------+
+(id, year) is the primary key (combination of columns with unique values) of this table.
+The table has information about the id and the year of each inventory and the corresponding net present value.
+ 
+
+Table: Queries
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| year          | int     |
++---------------+---------+
+(id, year) is the primary key (combination of columns with unique values) of this table.
+The table has information about the id and the year of each inventory query.
+ 
+
+Write a solution to find the npv of each query of the Queries table.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+NPV table:
++------+--------+--------+
+| id   | year   | npv    |
++------+--------+--------+
+| 1    | 2018   | 100    |
+| 7    | 2020   | 30     |
+| 13   | 2019   | 40     |
+| 1    | 2019   | 113    |
+| 2    | 2008   | 121    |
+| 3    | 2009   | 12     |
+| 11   | 2020   | 99     |
+| 7    | 2019   | 0      |
++------+--------+--------+
+Queries table:
++------+--------+
+| id   | year   |
++------+--------+
+| 1    | 2019   |
+| 2    | 2008   |
+| 3    | 2009   |
+| 7    | 2018   |
+| 7    | 2019   |
+| 7    | 2020   |
+| 13   | 2019   |
++------+--------+
+Output: 
++------+--------+--------+
+| id   | year   | npv    |
++------+--------+--------+
+| 1    | 2019   | 113    |
+| 2    | 2008   | 121    |
+| 3    | 2009   | 12     |
+| 7    | 2018   | 0      |
+| 7    | 2019   | 0      |
+| 7    | 2020   | 30     |
+| 13   | 2019   | 40     |
++------+--------+--------+
+Explanation: 
+The npv value of (7, 2018) is not present in the NPV table, we consider it 0.
+The npv values of all other queries can be found in the NPV table.
+*/
+select q.id,q.year, coalesce(n.npv,0) as npv from queries q left join
+npv n on (q.id =n.id) and (n.year=q.year);
+/*
+Table: Point
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| x           | int  |
++-------------+------+
+In SQL, x is the primary key column for this table.
+Each row of this table indicates the position of a point on the X-axis.
+ 
+
+Find the shortest distance between any two points from the Point table.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Point table:
++----+
+| x  |
++----+
+| -1 |
+| 0  |
+| 2  |
++----+
+Output: 
++----------+
+| shortest |
++----------+
+| 1        |
++----------+
+Explanation: The shortest distance is between points -1 and 0 which is |(-1) - 0| = 1.
+ 
+
+Follow up: How could you optimize your solution if the Point table is ordered in ascending order?
+*/
+WITH RankedPoints AS (
+    SELECT 
+        x, 
+        RANK() OVER (ORDER BY x) AS rn 
+    FROM 
+        Point
+)
+SELECT 
+    MIN(ABS(p1.x - p2.x)) AS shortest
+FROM 
+    RankedPoints p1
+CROSS JOIN 
+    RankedPoints p2
+WHERE 
+    p1.rn < p2.rn;
+/* 
+Table: Olympic
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| country       | varchar |
+| gold_medals   | int     |
+| silver_medals | int     |
+| bronze_medals | int     |
++---------------+---------+
+In SQL, country is the primary key for this table.
+Each row in this table shows a country name and the number of gold, silver, and bronze medals it won in the Olympic games.
+ 
+
+The Olympic table is sorted according to the following rules:
+
+The country with more gold medals comes first.
+If there is a tie in the gold medals, the country with more silver medals comes first.
+If there is a tie in the silver medals, the country with more bronze medals comes first.
+If there is a tie in the bronze medals, the countries with the tie are sorted in ascending order lexicographically.
+Write a solution to sort the Olympic table.
+
+The result format is shown in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Olympic table:
++-------------+-------------+---------------+---------------+
+| country     | gold_medals | silver_medals | bronze_medals |
++-------------+-------------+---------------+---------------+
+| China       | 10          | 10            | 20            |
+| South Sudan | 0           | 0             | 1             |
+| USA         | 10          | 10            | 20            |
+| Israel      | 2           | 2             | 3             |
+| Egypt       | 2           | 2             | 2             |
++-------------+-------------+---------------+---------------+
+Output: 
++-------------+-------------+---------------+---------------+
+| country     | gold_medals | silver_medals | bronze_medals |
++-------------+-------------+---------------+---------------+
+| China       | 10          | 10            | 20            |
+| USA         | 10          | 10            | 20            |
+| Israel      | 2           | 2             | 3             |
+| Egypt       | 2           | 2             | 2             |
+| South Sudan | 0           | 0             | 1             |
++-------------+-------------+---------------+---------------+
+Explanation: 
+The tie between China and USA is broken by their lexicographical names. Since "China" is lexicographically smaller than "USA", it comes first.
+Israel comes before Egypt because it has more bronze medals.
+*/
+select * from Olympic order by gold_medals desc, silver_medals desc,
+bronze_medals desc, country asc; 
+/*
