4ff16e41d54ef1b66be256b9740fc91788ecdbbb
Sreedhar <96693636+sree2798@users.noreply.github.com>
2024-04-18 23:32:12 -0500

    Update Day_13.sql

--- Day_13.sql
+++ Day_13.sql
@@ -615,3 +615,51 @@ Output table is ordered by user_id in ascending order.
 */
 select t1.user_id from (select user_id, group_concat(loan_type) as loan_type from loans group by
 user_id) as t1 where (t1.loan_type LIKE "%Mortgage%" and loan_type LIKE "%Refinance%");
+
+/*
+Table: Orders
+
++-------------------+------+
+| Column Name       | Type |
++-------------------+------+
+| order_id          | int  |
+| item_count        | int  |
+| order_occurrences | int  |
++-------------------+------+
+order_id is column of unique values for this table.
+This table contains order_id, item_count, and order_occurrences.
+Write a solution to calculate the average number of items per order, rounded to 2 decimal places.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Orders table:
++----------+------------+-------------------+
+| order_id | item_count | order_occurrences | 
++----------+------------+-------------------+
+| 10       | 1          | 500               | 
+| 11       | 2          | 1000              |     
+| 12       | 3          | 800               |  
+| 13       | 4          | 1000              | 
++----------+------------+-------------------+
+Output
++-------------------------+
+| average_items_per_order | 
++-------------------------+
+| 2.70                    |
++-------------------------+
+Explanation
+The calculation is as follows:
+ - Total items: (1 * 500) + (2 * 1000) + (3 * 800) + (4 * 1000) = 8900 
+ - Total orders: 500 + 1000 + 800 + 1000 = 3300 
+ - Therefore, the average items per order is 8900 / 3300 = 2.70
+ */
+select  round(sum(item_count*order_occurrences)/ sum(order_occurrences),2) as
+average_items_per_order
+from orders;
